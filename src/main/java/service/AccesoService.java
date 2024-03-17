00c82dd41c584a02571e0de584dbb71d79d9083f
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2024-03-17 16:46:21 -0500

    async call is implemented

---
+++ src/main/java/service/AccesoService.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.scheduling.annotation.Async;
+
+import model.Persona;
+
+public interface AccesoService {
+
+	CompletableFuture<List<Persona>> llamadaServicio(Persona persona);
+
+}
